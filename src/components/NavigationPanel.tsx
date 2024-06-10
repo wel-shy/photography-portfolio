5cbb7b4d7bb9d7fc9c25b1bc0bb55565da4f573e
Daniel Welsh <danjwelsh24@gmail.com>
2024-06-10 20:07:07 +0200

    updating navigation

--- src/components/NavigationPanel.tsx
+++ src/components/NavigationPanel.tsx
@@ -5,34 +5,44 @@ const Wrapper = styled.div`
   min-width: 20vw;
   ul {
     list-style-type: none;
+    padding-left: 0;
+  }
+`;
+
+const NavigationItem = styled.li<{ isActive?: boolean }>`
+  transition: ease all 0.2s;
+  margin-bottom: 0.5em;
+  font-weight: ${({ isActive }) => (isActive ? 700 : 400)};
+
+  a {
+    text-decoration: none;
+    color: #020202;
   }
 
-  li {
-    transition: ease all 0.2s;
-    margin-bottom: 0.5em;
-
-    a {
-      text-decoration: none;
-      color: #020202;
-    }
-  }
-
-  li:hover {
+  &:hover {
     cursor: pointer;
     font-weight: bold;
   }
 `;
 
-const NavigationPanel = () => {
+const useNavigationPanelController = () => {
   const { collections } = useCollectionsContext();
+
+  return {
+    collections,
+  };
+};
+
+const NavigationPanel = () => {
+  const { collections } = useNavigationPanelController();
 
   return (
     <Wrapper>
       <ul>
         {collections.map(({ id, title }) => (
-          <li key={id}>
+          <NavigationItem key={id}>
             <a href={`/collections/${id}`}>{title}</a>
-          </li>
+          </NavigationItem>
         ))}
       </ul>
     </Wrapper>
