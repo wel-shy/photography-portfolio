380b95616dff10d059b7e77f2221935f5e0bbf96
Daniel Welsh <danjwelsh24@gmail.com>
2024-06-15 11:02:05 +0200

    updating footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,19 @@
 import styled from "styled-components";
 
 const StyledFooter = styled.footer`
-  border-top: 1px solid grey;
   padding-top: 1em;
   font-style: italic;
-  margin-top: 2em;
 `;
 
 const Footer = () => {
   return (
     <StyledFooter>
+      <hr />
       <div>All content and photographs &copy; Daniel Welsh 2024</div>
+      <div>
+        For all enquiries:{" "}
+        <a href="mailto:photography@wel-shy.com">photography@wel-shy.com</a>
+      </div>
     </StyledFooter>
   );
 };
