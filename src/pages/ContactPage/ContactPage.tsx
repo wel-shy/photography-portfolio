232f1ad6fc26668701015315742532e62bb2c86c
Daniel Welsh <danjwelsh24@gmail.com>
2025-04-10 12:53:12 +0200

    adding contact info

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -1,7 +1,19 @@
+import styled from "styled-components"
 import BasePage from "../../components/BasePage/BasePage"
 
+const ContactLink = styled.div`
+    cursor: pointer;
+    margin-bottom: 1em;
+`
+
 const ContactPage = () => {
-    return <BasePage><h1>Contact</h1></BasePage>
+    return (
+    <BasePage>
+        <h1>Contact</h1>
+        <ContactLink><a target="_blank" href="https://www.instagram.com/wel__shy/">Instagram</a></ContactLink>
+        <ContactLink><a target="_blank" href="mailto:photography@welshy.net">Email</a></ContactLink>
+        <ContactLink><a target="_blank" href="https://bsky.app/profile/wel-shy.bsky.social">BlueSky</a></ContactLink>
+    </BasePage>)
 }
 
 export default ContactPage
